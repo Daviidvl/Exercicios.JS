86db18d88d17843d70ce3e2f1004dc836fe3f4e7
Daviidvl <davidovalee@icloud.com>
2024-12-10 21:54:36 -0300

    novo

---
+++ Cadastro de imoveis/index.js
@@ -0,0 +1,14 @@
+const imoveis = []
+let opção = ""
+
+do{
+opção = prompt(
+    "Bem vindo ao cadastro de imoveis. \n Total de imoveis: " + imoveis.length +
+    "\n\n Escolha uma opção: \n1. Novo imovel\n2. Listar imoveis\n3. Sair"
+)
+
+switch(opção){
+    case "1":
+        
+}
+}while(opção !== "3")
